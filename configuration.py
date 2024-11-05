c8b2c6ee0de9b8eac0314c46472748192dc40057
Monica <moniklunah0189@gmail.com>
2024-11-04 22:15:35 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://cnt-62c2297f-007f-4acd-9f91-f6435ba4bad7.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+
